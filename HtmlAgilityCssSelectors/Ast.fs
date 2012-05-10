e2cca4b6b4f91900d196032a884e26c2459255a2
unknown <james.luck@CRO-DSK-082.dotdigitalgroup.plc>
2012-05-10 10:11:44 +0100

    _

---
+++ HtmlAgilityCssSelectors/Ast.fs
@@ -0,0 +1,59 @@
+﻿#light
+
+namespace Ast
+open System
+
+
+//http://www.w3.org/TR/css3-selectors/#w3cselgrammar
+
+
+
+//type PseudoClass = 
+//    | AttrEquals of string
+//
+//type SimpleSelector =
+//    | Class of string
+//    | Id of string
+//    | Attrib
+//    | Negation
+//
+//type SequenceStart =
+//    | Universal
+//    | Tag of string
+//
+//type SequencePrefix =
+//    | Universal
+//    | Tag of string
+//
+//type Sequence = 
+//    | Prefix of SequencePrefix
+//    | Single of SequencePrefix * SimpleSelector
+//    | Many of SequencePrefix * SimpleSelector list
+//
+//type Selector = Single of string
+//    | Single of Sequence
+//    | Combined of Sequence * Combinator * Selector
+
+
+
+type combinator = 
+    | Descendant 
+    | Child
+
+
+type simpleSelectors = 
+    | Global
+    | Tag of string
+    | Class of string
+    | Id of string
+
+type simpleSelectorSeq = 
+    | SimpleSelectorSeq of simpleSelectors list
+
+type selector = 
+    | Selector of simpleSelectorSeq
+    | SelectorSequence of simpleSelectorSeq * combinator * selector
+
+type selectorsGroup =
+    | SelectorsGroup of selector list
+
---
+++ HtmlAgilityCssSelectors/Program.fs
@@ -0,0 +1,25 @@
+﻿
+open System
+open Microsoft.FSharp.Text.Lexing
+
+open Ast
+open Lexer
+open Parser
+open System.Diagnostics
+
+
+let time f = 
+    let timer = Stopwatch.StartNew()
+    let r = f()
+    timer.Stop()
+    printfn "Took %d ms, %d ticks, frequency = %d" timer.ElapsedMilliseconds timer.ElapsedTicks Stopwatch.Frequency
+    r
+
+let lexbuff = LexBuffer<char>.FromString("E.this.class")
+
+
+let result = time (fun ()-> Parser.start Lexer.tokenize lexbuff)
+
+let lexbuff2 = LexBuffer<char>.FromString("*.every.class")
+let result2 = time (fun ()-> Parser.start Lexer.tokenize lexbuff2)
+let x = 1       
